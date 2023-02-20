0ed1f471e2bf50bf2699909ad3324c99bb501ebb
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-20 14:35:54 +0100

    Implementing the DataContext

---
+++ udemy-dotnet5-rpg/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using udemy_dotnet5_rpg.Models;
+
+namespace udemy_dotnet5_rpg.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Character> Characters { get; set; }
+	}
+}
