2cdddcdb6872f5f01b30954e34da092daddd6f70
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-21 09:04:32 +0100

    Authentication Repository

---
+++ udemy-dotnet5-rpg/Data/AuthRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using udemy_dotnet5_rpg.Models;
+
+namespace udemy_dotnet5_rpg.Data
+{
+	public class AuthRepository : IAuthRepository
+	{
+		public Task<ServiceResponse<string>> Login(User user, string password)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<ServiceResponse<int>> Register(User user, string password)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<bool> UserExists(string username)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ udemy-dotnet5-rpg/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using udemy_dotnet5_rpg.Models;
+
+namespace udemy_dotnet5_rpg.Data
+{
+	public interface IAuthRepository
+	{
+		Task<ServiceResponse<int>> Register(User user, string password);
+		Task<ServiceResponse<string>> Login(User user, string password);
+		Task<bool> UserExists(string username);
+	}
+}
